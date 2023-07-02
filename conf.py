13befd944dcc9ae512192df8ba5f03b3f18a8ee9
GatoHery <54414691+GatoHery@users.noreply.github.com>
2023-07-01 23:59:22 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -13,11 +13,8 @@ release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-import myst_parser 
-extensions = [
-    'myst_parser',
-]
+ 
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
